114d7c0604e99e1bfee6d4ed51b2f27b97191ff9
SEC-MASUM <masum.jess42@gmail.com>
2022-04-08 08:07:59 +0600

    used CountContext in FirstComponentChild, SecondComponent and ThirdComponent

--- src/Components/SecondComponent/SecondComponent.js
+++ src/Components/SecondComponent/SecondComponent.js
@@ -1,19 +1,20 @@
-import React from "react";
-
+import React, { useContext } from "react";
 import { GrFormAdd } from "react-icons/gr";
 import { AiOutlineMinus } from "react-icons/ai";
+import { CountContext } from "../../App";
 
 const SecondComponent = () => {
+  const [count, setCount] = useContext(CountContext);
   return (
     <div className="second-component">
       <div className="counter-container">
         <h1>Second Component</h1>
         <div>
-          <button title="Increase">
+          <button title="Increase" onClick={() => setCount(count + 1)}>
             <GrFormAdd size={20} />
           </button>
-          <h1>Count: </h1>
-          <button title="Decrease">
+          <h1>Count: {count}</h1>
+          <button title="Decrease" onClick={() => setCount(count - 1)}>
             <AiOutlineMinus size={20} />
           </button>
         </div>
--- src/Components/ThirdComponent/ThirdComponent.js
+++ src/Components/ThirdComponent/ThirdComponent.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GrFormAdd } from "react-icons/gr";
 import { AiOutlineMinus } from "react-icons/ai";
+import { CountContext } from "../../App";
 
 const ThirdComponent = () => {
+  const [count, setCount] = useContext(CountContext);
   return (
     <div className="third-component">
       <div className="counter-container">
         <h1>Third Component</h1>
         <div>
           <button title="Increase">
-            <GrFormAdd size={20} />
+            <GrFormAdd size={20} onClick={() => setCount(count + 1)} />
           </button>
-          <h1>Count: </h1>
+          <h1>Count: {count}</h1>
           <button title="Decrease">
-            <AiOutlineMinus size={20} />
+            <AiOutlineMinus size={20} onClick={() => setCount(count - 1)} />
           </button>
         </div>
       </div>

cc5a0d6db8e55524276ce06b549d7fac6f6e575b
SEC-MASUM <masum.jess42@gmail.com>
2022-04-08 08:00:16 +0600

    implemented CountContext in FirstComponent successfully

--- src/Components/FirstComponent/FirstComponent.js
+++ src/Components/FirstComponent/FirstComponent.js
@@ -5,19 +5,19 @@ import { AiOutlineMinus } from "react-icons/ai";
 import { CountContext } from "../../App";
 
 const FirstComponent = () => {
-  const x = useContext(CountContext);
+  const [count, setCount] = useContext(CountContext);
 
-  console.log(x);
+  console.log(count);
   return (
     <div className="first-component">
       <div className="counter-container">
         <h1>First Component</h1>
         <div>
-          <button title="Increase">
+          <button title="Increase" onClick={() => setCount(count + 1)}>
             <GrFormAdd size={20} />
           </button>
-          <h1>Count:</h1>
-          <button title="Decrease">
+          <h1>Count: {count}</h1>
+          <button title="Decrease" onClick={() => setCount(count - 1)}>
             <AiOutlineMinus size={20} />
           </button>
         </div>
